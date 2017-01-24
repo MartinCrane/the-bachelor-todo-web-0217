2f7d06e36939360a525775a50214978ff7aaafc1
Martin Crane <martin.crane@gmail.com>
2017-01-24 21:31:24 +0000

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,13 +1,35 @@
+require "pry"
+
 def get_first_name_of_season_winner(data, season)
-  # code here
+  data["#{season}"].each do |contestant|
+    if contestant["status"] == "Winner"
+      return contestant["name"].split(" ")[0]
+    end
+  end
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  data.each do |season, season_data|
+    season_data.each do |character|
+
+      if character["occupation"] == occupation
+        return character["name"]
+      end
+    end
+  end
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  hometown_count = 0
+  data.each do |season, season_data|
+    season_data.each do |character, character_data|
+      
+      if character["hometown"] == hometown
+        hometown_count += 1
+      end
+    end
+  end
+  hometown_count
 end
 
 def get_occupation(data, hometown)
